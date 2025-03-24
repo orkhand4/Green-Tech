4ecef2ad8e8ce7873eb63a6d31fc800b8826e3a5
Orkhand4 <orkhand4@mail.ru>
2025-03-24 21:17:14 +0400

    Footer updated

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -7,34 +7,58 @@ const Footer = () => {
         {/* Loqo və Qısa Məlumat */}
         <div className="flex gap-5 mb-6 md:mb-0 text-center md:text-left">
           <Link to="/">
-            <img src="/src/assets/greentechno.svg" alt="GreenTech" className="h-12 w-30 md:h-28 mx-auto md:mx-0" />
+            <img
+              src="/src/assets/greentechno.svg"
+              alt="GreenTech"
+              className="h-12 w-30 md:h-28 mx-auto md:mx-0"
+            />
           </Link>
           <p className=" max-w-sm">
-            GreenTech ekoloji texnologiyalara və startaplara dəstək verən platformadır. Gəlin birlikdə dünyanı daha yaşıl edək!
+            GreenTech ekoloji texnologiyalara və startaplara dəstək verən
+            platformadır. Gəlin birlikdə dünyanı daha yaşıl edək!
           </p>
         </div>
 
         {/* Naviqasiya Linkləri */}
         <div className="flex flex-wrap justify-center gap-6 text-lg">
-          <Link to="/startups" className="hover:text-green-300 transition">Startaplar</Link>
-          <Link to="/investors" className="hover:text-green-300 transition">İnvestorlar</Link>
-          <Link to="/events" className="hover:text-green-300 transition">Tədbirlər</Link>
-          <Link to="/resources" className="hover:text-green-300 transition">Resurslar</Link>
-          <Link to="/about" className="hover:text-green-300 transition">Haqqımızda</Link>
+          <Link to="/startups" className="hover:text-green-300 transition">
+            Startaplar
+          </Link>
+          <Link to="/investors" className="hover:text-green-300 transition">
+            İnvestorlar
+          </Link>
+          <Link to="/events" className="hover:text-green-300 transition">
+            Tədbirlər
+          </Link>
+          <Link to="/about" className="hover:text-green-300 transition">
+            Haqqımızda
+          </Link>
         </div>
 
         {/* Sosial Media İkonları */}
         <div className="mt-6 md:mt-0 flex space-x-4">
-          <a href="#" className="text-green-300 hover:text-green-100 transition">
+          <a
+            href="#"
+            className="text-green-300 hover:text-green-100 transition"
+          >
             <i className="fab fa-facebook text-2xl"></i>
           </a>
-          <a href="#" className="text-green-300 hover:text-green-100 transition">
+          <a
+            href="#"
+            className="text-green-300 hover:text-green-100 transition"
+          >
             <i className="fab fa-twitter text-2xl"></i>
           </a>
-          <a href="#" className="text-green-300 hover:text-green-100 transition">
+          <a
+            href="#"
+            className="text-green-300 hover:text-green-100 transition"
+          >
             <i className="fab fa-linkedin text-2xl"></i>
           </a>
-          <a href="#" className="text-green-300 hover:text-green-100 transition">
+          <a
+            href="#"
+            className="text-green-300 hover:text-green-100 transition"
+          >
             <i className="fab fa-instagram text-2xl"></i>
           </a>
         </div>
